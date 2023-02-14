08bcb5e38cabae4922a1722a0ce20a76d8e2f435
siddht1 <rt1yrru+github@gmail.com>
2023-02-14 12:24:38 +0530

    Update script.js
    Added download method to save all the available VOICES

--- script.js
+++ script.js
@@ -49,4 +49,13 @@ window.onload = () => {
     alert("Text-to-speech is not supported on your browser!"); 
   }
   console.log(_voices_avail);
+  //added download method
+  function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+download(_voices_avail, 'js_tts.txt', 'text/plain');
 };
