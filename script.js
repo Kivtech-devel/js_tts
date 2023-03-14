d43d115cf60fb414475a0b1bb2f253da3f4689e6
siddht1 <rt1yrru+github@gmail.com>
2023-03-14 22:18:50 +0530

    Update script.js
    Adding image api script

--- script.js
+++ script.js
@@ -98,3 +98,39 @@ const sendDataToServer = (url,data) => {
     console.error('Error sending voices data:', error);
   });
 };
+
+  
+  
+  __url='https://kivtech-devel.github.io/random_img_json/'
+// updated url
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function make_style(img_url)
+{
+  //css to randomize 
+// html {
+//   background: #ecf0f1 url(img_url) no-repeat center center fixed;
+//   background-size: cover;
+// }
+
+document.body.style.background = "#f3f3f3 url("+img_url+") no-repeat center center fixed";
+document.body.style.background_size="contain";
+    // document.getElementById('page_body').className="bg"+img_url;
+
+}
+function show_data(data)
+{
+  _rand=randomIntFromInterval(0,data.length-1);
+  console.log(data[_rand]);
+  _img=data[_rand];
+  console.log(_img); //image data , now send to make style 
+  make_style(_img)
+  
+}
+
+fetch(__url+'img.json')
+.then((response) => response.json())
+.then((data) => {
+  show_data(data.images)
+});
